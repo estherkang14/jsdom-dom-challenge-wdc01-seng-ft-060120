945a8c3a3d0659fff5573bdf20e213f59a633383
estherkang14 <estherkang14@gmail.com>
2020-07-15 14:37:10 -0400

    got count working + plus and minus buttons

---
+++ js/challenge.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const counterElement = document.getElementById("counter")
+    let counterInterval = setInterval(counterAction, 1000)
+
+    function counterAction() {
+        counterElement.innerHTML = parseInt(counterElement.innerHTML, 10) + 1 
+    }
+
+    document.addEventListener("click", function(e){
+        console.log(e.target.id)
+        if (e.target.id === "minus") {
+            counterElement.innerHTML = parseInt(counterElement.innerHTML, 10) - 1
+        }
+        if (e.target.id === "plus") {
+            counterElement.innerHTML = parseInt(counterElement.innerHTML, 10) + 1 
+        }
+        // if (e.target.idName === "heart") {
+        //     const likeList = document.querySelector(".likes")
+        //     const likeEl = document.createElement("li")
+
+        //     likeEl.innerText = 
+        // }
+        if (e.target.id === "pause") {
+            clearInterval(counterInterval)
+            e.target.innerHTML = "resume"
+            let currentCount = counterElement.innerHTML
+            console.log(currentCount)
+        }
+        
+    })
+    
+})
